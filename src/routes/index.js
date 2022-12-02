1ec7df1f1c031a37f23571f1afcc6251189cd3d3
chanjeong park <ckswjd99@snu.ac.kr>
2022-12-02 14:38:03 +0900

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,42 +12,44 @@ router.get('/health', (req, res) => {
 
 
 router.post('/Response.Restaurant', (req, res) => {
-  let requestedCategory = ''
+  let requestedCategory = requestedCategory = req.body.action.parameters?.food_category.value
   let response = {
     "version": "2.0",
     "resultCode": "OK",
     "output": Object.fromEntries(Object.entries(req.body.action.parameters).map(([key, val]) => [key, val.value]))
   }
-  if (requestedCategory = req.body.action.parameters?.food_category.value) {
-    switch (requestedCategory) {
-      case '한식': {
-        response.output.restaurant_list = '엄마손칼국수, 기절초풍왕순대'
-        break;
-      }
-      case '양식': {
-        response.output.restaurant_list = '오른손 푸드카페'
-        break;
-      }
-      case '일식': {
-        response.output.restaurant_list = '누들하우스'
-        break;
-      }
-      case '중식': {
-        response.output.restaurant_list = '흑룡강'
-        break;
-      }
-      case '아시안': {
-        response.output.restaurant_list = '딸랏롯빠이'
-        break;
-      }
-      case '분식': {
-        response.output.restaurant_list = '동대문 엽기떡볶이'
-        break;
-      }
-      case '비건': {
-        response.output.restaurant_list = '샐러드로우'
-        break;
-      }
+  console.log(requestedCategory)
+  switch (requestedCategory) {
+    case '한식': {
+      response.output.restaurant_list = '엄마손칼국수, 기절초풍왕순대'
+      break;
+    }
+    case '양식': {
+      response.output.restaurant_list = '오른손 푸드카페'
+      break;
+    }
+    case '일식': {
+      response.output.restaurant_list = '누들하우스'
+      break;
+    }
+    case '중식': {
+      response.output.restaurant_list = '흑룡강'
+      break;
+    }
+    case '아시안': {
+      response.output.restaurant_list = '딸랏롯빠이'
+      break;
+    }
+    case '분식': {
+      response.output.restaurant_list = '동대문 엽기떡볶이'
+      break;
+    }
+    case '비건': {
+      response.output.restaurant_list = '샐러드로우'
+      break;
+    }
+    default: {
+      response.output.restaurant_list = '없음'
     }
   }
   res.send(response)
