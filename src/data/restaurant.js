5879ad10110e60ef1218f175e466dc9aa90252d0
chanjeong park <ckswjd99@snu.ac.kr>
2022-12-02 15:39:38 +0900

    temp data

--- src/data/restaurant.js
+++ src/data/restaurant.js
@@ -3,6 +3,7 @@ module.exports = [
     category: '한식',
     name: '엄마손칼국수',
     address: '서울 관악구 관악로14길 105 (우)08789',
+    phone: '02-873-4613',
     menu: [
       {
         menu_name: '엄마손 칼국수',
@@ -22,6 +23,7 @@ module.exports = [
     category: '한식',
     name: '기절초풍왕순대',
     address: '서울 관악구 봉천로62길 2 1,2층 (우)08789',
+    phone: '02-872-8805',
     menu: [
       {
         menu_name: '순대국',
@@ -34,6 +36,118 @@ module.exports = [
       {
         menu_name: '순대정식',
         price: 16000
+      },
+    ]
+  },
+  {
+    category: '양식',
+    name: '오른손푸드카페',
+    address: '서울 관악구 관악로14길 111 1층 (우)08789',
+    phone: '02-803-6223',
+    menu: [
+      {
+        menu_name: '케이준 치킨 크림파스타',
+        price: 16800
+      },
+      {
+        menu_name: '알리오 올리오',
+        price: 17800
+      },
+      {
+        menu_name: '매콤한 해산물 토마토파스타',
+        price: 18800
+      },
+    ]
+  },
+  {
+    category: '중식',
+    name: '흑룡강',
+    address: '서울 관악구 낙성대로 22-1 (우)08790',
+    phone: '02-886-8556',
+    menu: [
+      {
+        menu_name: '짜장면',
+        price: 7000
+      },
+      {
+        menu_name: '짬뽕',
+        price: 8000
+      },
+      {
+        menu_name: '탕수육',
+        price: 20000
+      },
+    ]
+  },
+  {
+    category: '일식',
+    name: '누들하우스',
+    address: '서울 관악구 관악로14나길 6 1층 (우)08789',
+    phone: '070-8624-8497',
+    menu: [
+      {
+        menu_name: '돼지 육수 라멘',
+        price: 9000
+      },
+      {
+        menu_name: '닭 육수 라멘',
+        price: 9000
+      },
+      {
+        menu_name: '치킨 가라아게',
+        price: 6000
+      },
+    ]
+  },
+  {
+    category: '아시안',
+    name: '딸랏롯빠이',
+    address: '서울 관악구 관악로12길 113 대원빌딩 1층 101호 (우)08789',
+    phone: '0507-1464-4558',
+    menu: [
+      {
+        menu_name: '꾸에이띠어우 느아',
+        price: 10000
+      },
+    ]
+  },
+  {
+    category: '분식',
+    name: '동대문엽기떡볶이 서울대입구점',
+    address: '서울 관악구 남부순환로 1868 1층 (우)08789',
+    phone: '02-882-8591',
+    menu: [
+      {
+        menu_name: '엽기떡볶이',
+        price: 14000
+      },
+      {
+        menu_name: '로제떡볶이',
+        price: 16000
+      },
+      {
+        menu_name: '크림떡볶이',
+        price: 15000
+      },
+    ]
+  }
+  {
+    category: '채식',
+    name: '샐러드로우&트라타 서울대입구점',
+    address: '서울 관악구 남부순환로 1868 1층 102호 (우)08789',
+    phone: '02-882-6702',
+    menu: [
+      {
+        menu_name: '그릴드 목살샐러드 로우',
+        price: 7500
+      },
+      {
+        menu_name: '그릴드 닭가슴살샐러드 로우',
+        price: 7500
+      },
+      {
+        menu_name: '그릴드 닭다리살샐러드 로우',
+        price: 7500
       },
     ]
   }
