efa10f6937c201f092076274074760be275d31dc
chanjeong park <ckswjd99@snu.ac.kr>
2022-12-02 13:54:52 +0900

    test response

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.post('/Response.Restaurant', (req, res) => {
+  console.log(req)
+  res.send({
+    'hi': 'hello'
+  })
+})
+
 module.exports = router;
--- src/routes/restaurant.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router()
-
-router.post('/Response.Restaurant', (req, res) => {
-  console.log(req)
-  res.send({
-    'hi': 'hello'
-  })
-})
