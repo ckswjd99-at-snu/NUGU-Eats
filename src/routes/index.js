00beeec1a0e1017b823e45f91a45efd3bf98f774
chanjeong park <ckswjd99@snu.ac.kr>
2022-12-02 14:21:08 +0900

    test restaurant list

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,10 +8,47 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/Response.Restaurant', (req, res) => {
-  console.log(req.body)
-  res.send({
-    'hi': 'hello'
-  })
+  let requestedCategory = ''
+  let response = {
+    "version": "2.0",
+    "resultCode": "OK",
+    "output": {
+      "restaurant_list": "엄마손칼국수, 기절초풍왕순대",
+    },
+  }
+  if (requestedCategory = req.body.action?.food_category.value) {
+    switch (requestedCategory) {
+      case '한식': {
+        response.output.restaurant_list = '엄마손칼국수, 기절초풍왕순대'
+        break;
+      }
+      case '양식': {
+        response.output.restaurant_list = '오른손 푸드카페'
+        break;
+      }
+      case '일식': {
+        response.output.restaurant_list = '누들하우스'
+        break;
+      }
+      case '중식': {
+        response.output.restaurant_list = '흑룡강'
+        break;
+      }
+      case '아시안': {
+        response.output.restaurant_list = '딸랏롯빠이'
+        break;
+      }
+      case '분식': {
+        response.output.restaurant_list = '동대문 엽기떡볶이'
+        break;
+      }
+      case '비건': {
+        response.output.restaurant_list = '샐러드로우'
+        break;
+      }
+    }
+  }
+  res.send(response)
 })
 
 module.exports = router;
