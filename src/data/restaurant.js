43d2136dfb82e0099c0a8b6e0785a322bd702162
chanjeong park <ckswjd99@snu.ac.kr>
2022-12-02 15:02:04 +0900

    .

--- src/data/restaurant.js
+++ src/data/restaurant.js
@@ -1,42 +1,40 @@
-module.exports = {
-  list: [
-    {
-      category: '한식',
-      name: '엄마손칼국수',
-      address: '서울 관악구 관악로14길 105 (우)08789',
-      menu: [
-        {
-          menu_name: '엄마손 칼국수',
-          price: 6000
-        },
-        {
-          menu_name: '해물 칼국수',
-          price: 7000
-        },
-        {
-          menu_name: '떡만두국',
-          price: 6000
-        },
-      ]
-    },
-    {
-      category: '한식',
-      name: '기절초풍왕순대',
-      address: '서울 관악구 봉천로62길 2 1,2층 (우)08789',
-      menu: [
-        {
-          menu_name: '순대국',
-          price: 8000
-        },
-        {
-          menu_name: '순대술국',
-          price: 15000
-        },
-        {
-          menu_name: '순대정식',
-          price: 16000
-        },
-      ]
-    }
-  ]
-}
+module.exports = [
+  {
+    category: '한식',
+    name: '엄마손칼국수',
+    address: '서울 관악구 관악로14길 105 (우)08789',
+    menu: [
+      {
+        menu_name: '엄마손 칼국수',
+        price: 6000
+      },
+      {
+        menu_name: '해물 칼국수',
+        price: 7000
+      },
+      {
+        menu_name: '떡만두국',
+        price: 6000
+      },
+    ]
+  },
+  {
+    category: '한식',
+    name: '기절초풍왕순대',
+    address: '서울 관악구 봉천로62길 2 1,2층 (우)08789',
+    menu: [
+      {
+        menu_name: '순대국',
+        price: 8000
+      },
+      {
+        menu_name: '순대술국',
+        price: 15000
+      },
+      {
+        menu_name: '순대정식',
+        price: 16000
+      },
+    ]
+  }
+]
